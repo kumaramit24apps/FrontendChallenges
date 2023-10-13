5410f80ce763f6a0feef55de737a37a4ec567a12
Amit Kumar Pandit <kumaramit24apps@outlook.com>
2023-10-14 00:27:25 +0530

    Feat: Added the initial structure for the React & Tailwind Development

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "./style.css";
+
+console.log("working with javascript");
+for (let i = 0; i < 10; i++) {
+  console.log(`${i}`);
+}
+
+function formatTailwind() {
+  return `
+            <h1 class="py-4 text-3xl 
+            
+            text-slate-800  px-5  "> Heading </h1>
+            <p> Paragraph </p>
+        `;
+}
