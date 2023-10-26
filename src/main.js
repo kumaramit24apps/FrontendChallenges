198fa33b51540e7170f18d15e24f91366c32bef1
Amit Kumar Pandit <kumaramit24apps@outlook.com>
2023-10-26 13:02:01 +0530

    Refactor: removed .vscode setting from gitignore

--- src/main.js
+++ src/main.js
@@ -7,9 +7,7 @@ for (let i = 0; i < 10; i++) {
 
 function formatTailwind() {
   return `
-            <h1 class="py-4 text-3xl 
-            
-            text-slate-800  px-5  "> Heading </h1>
+            <h1 class=" text-3xl py-4 text-center text-slate-800  px-5  "> Heading </h1>
             <p> Paragraph </p>
         `;
 }
